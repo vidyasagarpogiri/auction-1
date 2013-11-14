615d6d42170cd6b589bd7b514220f6bd7f246623
oluckyman-pro <oluckyman@example.com>
2013-11-14 20:18:52 +0400

    Add User model with validations

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ActiveRecord::Base
+  validates :name, presense: true, length: { maximum: 35 }
+  # VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presense: true, 
+    # format: { with: VALID_EMAIL_REGEX }
+    uniqueness: { case_sensitive: false }
+end
