605b2df0fdd74a74819bc896cc84d2df02f3e812
oluckyman-pro <oluckyman@example.com>
2013-11-14 16:34:11 +0400

    Generate secret

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,20 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Auction::Application.config.secret_key_base = '3a76916bd7372b0c009ac45d0040dbf8811d9abaf05edf4608068fae88eeeab0e9d291698d00f40acd0b995ad6b269185b7c095aaf81e20bef13b2cf8151fc67'
+# Auction::Application.config.secret_key_base = '3a76916bd7372b0c009ac45d0040dbf8811d9abaf05edf4608068fae88eeeab0e9d291698d00f40acd0b995ad6b269185b7c095aaf81e20bef13b2cf8151fc67'
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+SampleApp::Application.config.secret_key_base = secure_token
